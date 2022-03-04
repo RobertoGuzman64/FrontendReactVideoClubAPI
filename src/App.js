a666dd09b5e824a313320400e1aa7c208aabe692
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-04 20:13:55 +0100

    Añadidas las rutas de las paginas que vamos a usar en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Contenedores/Home';
+import Login from './Contenedores/Login';
+import Pedidos from './Contenedores/Pedidos';
+import Peliculas from './Contenedores/Peliculas';
+import Perfil from './Contenedores/Perfil';
+import Registro from './Contenedores/Registro';
+import Admin from './Contenedores/Admin';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Login' element={<Login/>}/>
+        <Route path='/Pedidos' element={<Pedidos/>}/>
+        <Route path='/Peliculas' element={<Peliculas/>}/>
+        <Route path='/Perfil' element={<Perfil/>}/>
+        <Route path='/Registro' element={<Registro/>}/>
+        <Route path='/Admin' element={<Admin/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }

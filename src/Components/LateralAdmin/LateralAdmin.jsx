cfa5432adb6a1f5add7498833610d34fa203b4f3
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-16 17:23:07 +0100

    Añadido boton de desloguearse desde la vista administrador

--- src/Components/LateralAdmin/LateralAdmin.jsx
+++ src/Components/LateralAdmin/LateralAdmin.jsx
@@ -1,23 +1,44 @@
 import './LateralAdmin.css';
-// import React, { useEffect } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LOGOUT } from '../../redux/types';
+import { connect } from 'react-redux';
 
-const LateralAdmin = () => {
-    return(
+const LateralAdmin = (props) => {
+    // Usamos navigate para que cuando pulsemos logout nos redirija.
+    let navigate = useNavigate();
+    useEffect(() => {
+        console.log(props.credenciales)
+    })
+    // ESTA FUNCION HACE QUE PUEDAS DESLOGUEARTE
+    const logOut = () => {
+        //Borrar de RDX las credenciales
+        props.dispatch({ type: LOGOUT });
+        setTimeout(() => {
+            navigate("/");
+        }, 1500);
+    }
+    return (
         <div className='lateral'>
-        <div className='containerLogo'>
-            <div className='logo2'></div>
+            <div className='containerLogo'>
+                <div className='logo2'></div>
+            </div>
+            <div className='containerEndpoints'>
+            <h1 className='nombre'>{props.credenciales?.usuario.nombre}</h1>
+            <h1 className='nombre'>{props.credenciales?.usuario.apellidos}</h1>
+                <div className='endpointLateral'></div>
+                <div className='endpointLateral'></div>
+                <div className='endpointLateral'></div>
+                <div className='endpointLateral'></div>
+                <div className='endpointLateral' onClick={() => logOut()}>
+                    <h1 className='Letras'>CERRAR SESION</h1>
+                </div>
+            </div>
         </div>
-        <div className='containerEndpoints'>
-            <div className='endpointLateral'></div>
-            <div className='endpointLateral'></div>
-            <div className='endpointLateral'></div>
-            <div className='endpointLateral'></div>
-            <div className='endpointLateral'></div>
-        </div>
-    </div>
     )
 }
 
 
-export default LateralAdmin;
+export default connect((state) => ({
+    credenciales: state.credenciales
+}))(LateralAdmin);

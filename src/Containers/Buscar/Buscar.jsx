e922d4fa69f7a16182c122f54ea47a9689c3a95c
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-17 20:39:42 +0100

    Añadida la funcion de escoger Pelicula en la pagina Buscar y añadido el estado inicial de redux para poder buscar de nuevo cuando entres desde cualquier usuario diferente o incluso el mismo usuario

--- src/Containers/Buscar/Buscar.jsx
+++ src/Containers/Buscar/Buscar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // Redux
 import { connect } from 'react-redux';
-import { TITULO_PELICULA, ESTADO_INICIAL } from '../../redux/types';
+import { TITULO_PELICULA, ESTADO_INICIAL, DETALLE_PELICULA } from '../../redux/types';
 
 import './Buscar.css';
 import LateralUsuario from '../../Components/LateralUsuario/LateralUsuario';
@@ -10,13 +10,14 @@ import axios from 'axios';
 
 const Buscar = (props) => {
 
-
+    let resultados;
     let navigate = useNavigate();
     const [titulo, setTitulo] = useState("");
 
 
     useEffect(()=>{
         console.log(props.peliculas);
+        props.dispatch({ type: ESTADO_INICIAL });
     },[]);
 
 
@@ -35,19 +36,23 @@ const Buscar = (props) => {
     const busquedaPorTitulo = async () => {
         // Axios que trae resultados....
         try {
-            let resultados = await axios.get(`https://rgd-videoclub-backend.herokuapp.com/peliculas/titulo/${titulo}`);
+            resultados = await axios.get(`https://rgd-videoclub-backend.herokuapp.com/peliculas/titulo/${titulo}`);
             //Guardo en redux los resultados de las películas
             props.dispatch({ type: TITULO_PELICULA, payload: resultados.data });
-            // setTimeout(()=>{
-            //     props.dispatch({ type: ESTADO_INICIAL, payload: resultados.data }); esto tienes que poner cuando hagas el boton 
-            //     navigate("/DetallePelicula");                                       de cambio de pantalla
-            // },500);
         } catch (error) {
             console.log(error);
         }
     }
 
     
+    const escogePelicula = (pelicula) => {
+        console.log(pelicula);
+        props.dispatch({type: DETALLE_PELICULA, payload: pelicula});
+        //Redirigimos a la pagina DetallePelicula con navigate al Pulsar sobre una pelicula en concreto.
+        navigate("/detallePelicula");
+    };
+
+
     return (
         <div className='paginaBuscar'>
             <LateralUsuario />
@@ -66,7 +71,7 @@ const Buscar = (props) => {
                                 //Al mapear, cada elemento que se itera del array (en este caso pelicula es ese elemento),
                                 //si le hacemos propiedad onclick y pasamos el elemento como argumento,
                                 //a esa funcion le va a llegar el objeto que hayamos clickado entero
-                                <div className='cardPelicula' key={pelicula.id} /*onClick={() => escogePelicula(pelicula)}*/>
+                                <div className='cardPelicula' key={pelicula.id} onClick={() => escogePelicula(pelicula)}>
                                     <p className='Letras'>{pelicula.titulo}</p>
                                     <img className='imagenPelicula' src={pelicula.imagen} alt={pelicula.titulo} />
                                     <p>{pelicula.sinopsis}</p>
